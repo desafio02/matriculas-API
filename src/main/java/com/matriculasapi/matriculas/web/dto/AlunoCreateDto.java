e953c094d62707713a8adf1c2e7f48008e7552e5
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-05-07 11:06:25 -0300

    feat: Adição dos arquivos web

---
+++ src/main/java/com/matriculasapi/matriculas/web/dto/AlunoCreateDto.java
@@ -0,0 +1,39 @@
+package com.matriculasapi.matriculas.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlunoCreateDto {
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @CPF
+    @Size(min = 11, max = 11)
+    private String cpf;
+
+    @NotBlank
+    private Date dataNascimento;
+
+    @NotBlank
+    @Pattern(regexp = "M|F")
+    private String Sexo;
+
+    @NotBlank
+    private boolean ativo;
+}
+
+
